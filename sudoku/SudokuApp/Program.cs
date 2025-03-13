177b53a65c6b6df6de27b18d925b601676a8e4fb
janinaisonline <jani.herren@gmail.com>
2025-03-13 23:13:26 +0100

    added comment for better understanding of the file

--- sudoku/SudokuApp/Program.cs
+++ sudoku/SudokuApp/Program.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 // using SudokuApp.SudokuFunctionality;
 
+// this file contains everything to create an ASP.NET Core web application and start a web server, the controller file then handles the http requests
+
 var builder = WebApplication.CreateBuilder(args);
 
 // **1. Add services to the container**
