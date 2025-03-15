b24f3926b80072983b7ed3748d262df65ccaa221
janinaisonline <jani.herren@gmail.com>
2025-03-15 12:18:42 +0100

    auto updates

--- sudoku/SudokuApp/obj/Debug/net9.0/SudokuApp.AssemblyInfo.cs
+++ sudoku/SudokuApp/obj/Debug/net9.0/SudokuApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SudokuApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+226b4edb14b2150ba5e6a9a82f9a2ddc34ce1f22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de57b9acc24f1c5ecf6841a9c15c560814b1806f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SudokuApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SudokuApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
