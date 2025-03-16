3df8fe370cef10950ea7146549dcf1a2325ea4cc
janinaisonline <jani.herren@gmail.com>
2025-03-16 23:26:14 +0100

    current full sudoku generator

--- frontend/react-sudoku/src/App.js
+++ frontend/react-sudoku/src/App.js
@@ -2,33 +2,16 @@ import { useState, useEffect } from "react";
 import SudokuGrid from "./SudokuGrid";
 import './App.css';
 
-function SudokuGame() {
+function SudokuGame() { 
     const [key, setKey] = useState(0);
-    // const [sudokuGrid, setSudokuGrid] = useState(null);
-    // const [difficulty, setDifficulty] = useState("easy");
-
+   
     const regenerateSudoku = () => {
         setKey(prevKey => prevKey + 1); // Change the key to force re-render
     };
 
-    // const fetchSudoku = async (level) => {
-    //     try {
-    //         const response = await fetch(`http://localhost:5099/api/sudoku/${level}`);
-    //         const data = await response.json();
-    //         setSudokuGrid(data.grid);
-    //     } catch (error) {
-    //         console.error("Error fetching Sudoku:", error);
-    //     }
-    // };
-
-    // useEffect(() => {
-    //     fetchSudoku(difficulty);
-    // }, [difficulty]);
-
     return (
         <div className="sudoku-game">
             <h1>Sudoku Player</h1>
-            {/* {sudokuGrid ? <SudokuGrid grid={sudokuGrid} /> : <p>Loading...</p>} */}
             <SudokuGrid key={key}/>
             <button onClick={regenerateSudoku}>Generate new sudoku</button>
             <div className="difficulty-container">
