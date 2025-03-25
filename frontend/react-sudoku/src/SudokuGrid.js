f8d97bb55c4035fdaecca240e6fbd63ec42b544d
janinaisonline <jani.herren@gmail.com>
2025-03-25 23:05:28 +0100

    changes in sudoku style, proper sudoku grid now

--- frontend/react-sudoku/src/SudokuGrid.js
+++ frontend/react-sudoku/src/SudokuGrid.js
@@ -17,14 +17,40 @@ const SudokuGrid = ({ difficulty }) => {
     return (
         <div className="sudoku-container">
             <div className="sudoku-grid">
-                {grid.flat().map((num, index) => (
+            {grid.map((row, rowIndex) =>
+                    row.map((num, colIndex) => {
+                        // determine border thickness based on 3x3 grid layout
+                        const isTopBorder = rowIndex % 3 === 0;
+                        const isLeftBorder = colIndex % 3 === 0;
+                        const isBottomBorder = rowIndex === 8;
+                        const isRightBorder = colIndex === 8;
+
+                        const cellStyle = {
+                            borderTop: isTopBorder ? "2px solid black" : "1px solid black",
+                            borderLeft: isLeftBorder ? "2px solid black" : "1px solid black",
+                            borderRight: isRightBorder ? "2px solid black" : "",
+                            borderBottom: isBottomBorder ? "2px solid black" : "",
+                        };
+
+                        return (
+                            <div
+                                key={`${rowIndex}-${colIndex}`}
+                                className={`sudoku-cell ${num === 0 ? "empty" : ""}`}
+                                style={cellStyle}
+                            >
+                                {num !== 0 ? num : ""}
+                            </div>
+                        );
+                    })
+                )}
+                {/* {grid.flat().map((num, index) => (
                     <div
                         key={index}
                         className={`sudoku-cell ${num === 0 ? "empty" : ""}`}
                     >
                         {num !== 0 ? num : ""}
                     </div>
-                ))}
+                ))} */}
             </div>
         </div>
     );
